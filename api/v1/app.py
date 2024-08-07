385b047c62c9290c225b91dbb2aaa952340a3203
chinedu06 <godswillchinedu644@gmail.com>
2024-08-07 04:29:50 +0000

    first commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,3 +17,16 @@ if __name__ == "__main__":
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
 
+def close_storage(exception):
+    """Close the current SQLAlchemy session."""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors by returning a JSON response."""
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,21 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+def stats():
+    """Retrieves the number of each object by type"""
+    stats_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats_dict)
